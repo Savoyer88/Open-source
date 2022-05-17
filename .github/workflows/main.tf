a517a03499eb91d8a9833c7e20274fb555e8b6a1
Marlen Tleuberdinov <75469106+Savoyer88@users.noreply.github.com>
2022-05-16 21:58:12 -0600

    1

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -210,23 +210,3 @@ terraform {
     region = "us-east-1"
   }
 }
-
-terraform {
-  required_providers {
-    assert = {
-      source  = "bwoznicki/assert"
-      version = "0.0.1"
-    }
-  }
-}
-data "assert_test" "workspace" {
-  test  = terraform.workspace != "origin"
-  throw = "default workspace is not valid in this project"
-}
-
-data "aws_region" "current" {}
-
-data "assert_test" "region" {
-  test  = data.aws_region.current.name == "us-east-1"
-  throw = "You cannot deploy this resource in any other region but us-east-1"
-}
